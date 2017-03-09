0d9a2996f8cf0db539e9465556b9d656cce41076
caasdemo <janakiraman.r@prodapt.com>
2017-03-09 14:18:29 +0530

    Test case

--- example/tests.py
+++ example/tests.py
@@ -1,9 +1,9 @@
 from django.test import TestCase
 
 from example.models import example
-# Create your tests here.
+# Create your tests here.    
 class exampletest(TestCase):
-      def createexample(self,fname="janakiraman",fphone="9i76724389"):
+      def createexample(self,fname="janakiraman",fphone="9176724389"):
           return example.objects.create(name=fname,phone=fphone)
       def test_example(self):
           r=self.createexample()
